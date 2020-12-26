003024d51b584268f157231f8d7efdf650c5606e
Vincent Chen <vince_1728@hotmail.com>
2020-12-26 02:02:13 -0800

    trailer player

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from './../../axios';
 import Poster from './Poster/Poster';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 import './Category.css';
 
+const opts = {
+	height: '390',
+	width: '100%',
+	playerVars: {
+		autoplay: 1,
+	},
+};
+
 function Category({ title, fetchUrl, isLargeRow }) {
 	const [movies, setMovies] = useState([]);
+	const [trailerUrl, setTrailerUrl] = useState('');
 
 	useEffect(() => {
 		async function fetchData() {
@@ -17,9 +28,24 @@ function Category({ title, fetchUrl, isLargeRow }) {
 		fetchData();
 	}, [fetchUrl]);
 
+	const handleClick = movie => {
+		if (trailerUrl) {
+			setTrailerUrl('');
+		} else {
+			movieTrailer(
+				movie?.name || movie?.title || movie?.original_title || ''
+			)
+				.then(url => {
+					const urlParams = new URLSearchParams(new URL(url).search);
+					setTrailerUrl(urlParams.get('v'));
+				})
+				.catch(err => console.log(err.message));
+		}
+	};
+
 	return (
 		<div className="category">
-			<h2>{title}</h2>
+			<h2 className="category__title">{title}</h2>
 
 			<div className="category__posters">
 				{movies.map(movie => (
@@ -27,10 +53,11 @@ function Category({ title, fetchUrl, isLargeRow }) {
 						key={movie.id}
 						movie={movie}
 						isLargeRow={isLargeRow}
+						onClick={handleClick}
 					/>
 				))}
 			</div>
-			{/* container -> posters */}
+			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
 		</div>
 	);
 }
--- src/components/Category/Poster/Poster.js
+++ src/components/Category/Poster/Poster.js
@@ -3,12 +3,19 @@ import './Poster.css';
 
 const base_image_url = 'https://image.tmdb.org/t/p/original/';
 
-function Poster({ movie, isLargeRow }) {
+function Poster({ movie, isLargeRow, onClick }) {
 	const style = `poster ${isLargeRow && 'poster__large'}`;
 	const url =
 		base_image_url + (isLargeRow ? movie.poster_path : movie.backdrop_path);
 
-	return <img className={style} src={url} alt={movie.name} />;
+	return (
+		<img
+			className={style}
+			src={url}
+			alt={movie.name}
+			onClick={() => onClick(movie)}
+		/>
+	);
 }
 
 export default Poster;
