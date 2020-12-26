5b7eb36f753e01816bc78f8b889a1771d05a7163
Vincent Chen <vince_1728@hotmail.com>
2020-12-25 21:07:36 -0800

    refactor posters

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from './../../axios';
+import Poster from './Poster/Poster';
 
 import './Category.css';
-import Poster from './Poster/Poster';
 
 function Category({ title, fetchUrl, isLargeRow }) {
 	const [movies, setMovies] = useState([]);
--- src/components/Category/Poster/Poster.js
+++ src/components/Category/Poster/Poster.js
@@ -4,16 +4,11 @@ import './Poster.css';
 const base_image_url = 'https://image.tmdb.org/t/p/original/';
 
 function Poster({ movie, isLargeRow }) {
-	return (
-		<img
-			className={`poster ${isLargeRow && 'poster__large'}`}
-			src={
-				base_image_url +
-				(isLargeRow ? movie.poster_path : movie.backdrop_path)
-			}
-			alt={movie.name}
-		/>
-	);
+	const style = `poster ${isLargeRow && 'poster__large'}`;
+	const url =
+		base_image_url + (isLargeRow ? movie.poster_path : movie.backdrop_path);
+
+	return <img className={style} src={url} alt={movie.name} />;
 }
 
 export default Poster;
